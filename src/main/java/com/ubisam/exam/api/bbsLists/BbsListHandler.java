08265bfa31dc6abcd218a4f4245edbb0e9dceb44
조흥재 연구원 <heung@ubisam.com>
2026-03-18 18:02:14 +0900

    20260318-쿼리문 변경

--- src/main/java/com/ubisam/exam/api/bbsLists/BbsListHandler.java
+++ src/main/java/com/ubisam/exam/api/bbsLists/BbsListHandler.java
@@ -16,22 +16,21 @@ public class BbsListHandler {
     의문점: 그럼 여기서 가장 적합한 쿼리는 무엇이냐.
     해결방안: AI한테 도움을 받은 결과 사용자가 무엇을 검색해도
              결과값이 출력되게 만들면 된다고 함.
-             모든 조건을 분기로 가지는 쿼리 작성
+             만약 빈 문자열이면 쿼리 빌드
+             무언가 값이 들어오면 검색이 되게끔 변경
             
   */
   @HandleBeforeRead
   public void beforeSearch(BbsList bbsList, Specification<BbsList> spec){
-    String keyword = bbsList.getKeyword();
-    JpaSpecificationBuilder<BbsList> query = JpaSpecificationBuilder.of(BbsList.class);
-    query.where()
-      .or().eq("title", keyword)
-      .or().eq("count", keyword)
-      .or().eq("title", "")
-      .or().eq("count", "")
-      .or().like("title", "%" + keyword + "%")
-      .or().like("count", "%" + keyword + "%")
-      .or().like("title", "%게시판%")
-      .or().like("count", "%게시판%")
-      .build(spec);
+      String keyword = bbsList.getKeyword();
+      JpaSpecificationBuilder<BbsList> query = JpaSpecificationBuilder.of(BbsList.class);
+      if (keyword == null || keyword.isEmpty()) {
+          query.build(spec);
+      } else {
+          query.where()
+              .or().like("title", "%" + keyword + "%")
+              .or().like("count", "%" + keyword + "%")
+              .build(spec);
+      }
   }
 }
