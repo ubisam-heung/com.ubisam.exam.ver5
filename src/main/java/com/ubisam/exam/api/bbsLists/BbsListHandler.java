a6438ba2b300e94fa74b1107e44cd5d33dde3486
조흥재 연구원 <heung@ubisam.com>
2026-03-18 17:58:12 +0900

    20260318-handler 쿼리 수정

--- src/main/java/com/ubisam/exam/api/bbsLists/BbsListHandler.java
+++ src/main/java/com/ubisam/exam/api/bbsLists/BbsListHandler.java
@@ -14,17 +14,24 @@ import io.u2ware.common.data.rest.core.annotation.HandleBeforeRead;
 public class BbsListHandler {
   /*
     의문점: 그럼 여기서 가장 적합한 쿼리는 무엇이냐.
-    해결방안: AI한테 도움을 받은 결과 테스트 할 때 썼던 쿼리를 전부 만족하면 1개의 쿼리로 가장
-            적합한 쿼리를 Handler에 지정할 수 있다고 하기에 1개의 쿼리로 4개를 만족하는 쿼리 작성
+    해결방안: AI한테 도움을 받은 결과 사용자가 무엇을 검색해도
+             결과값이 출력되게 만들면 된다고 함.
+             모든 조건을 분기로 가지는 쿼리 작성
+            
   */
   @HandleBeforeRead
   public void beforeSearch(BbsList bbsList, Specification<BbsList> spec){
+    String keyword = bbsList.getKeyword();
     JpaSpecificationBuilder<BbsList> query = JpaSpecificationBuilder.of(BbsList.class);
     query.where()
-        .or().eq("title", bbsList.getKeyword())
-        .or().eq("title", "")
-        .or().like("title", "%" + bbsList.getKeyword() + "%")
-        .or().like("title", "%게시판%")
-        .build(spec);
+      .or().eq("title", keyword)
+      .or().eq("count", keyword)
+      .or().eq("title", "")
+      .or().eq("count", "")
+      .or().like("title", "%" + keyword + "%")
+      .or().like("count", "%" + keyword + "%")
+      .or().like("title", "%게시판%")
+      .or().like("count", "%게시판%")
+      .build(spec);
   }
 }
